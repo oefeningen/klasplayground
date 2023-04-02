5a325968a8ea252878c4ab0e84fcb739a485b140
Kurt Latre <meester.kurt@gmail.com>
2023-04-02 17:54:31 +0200

    test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,83 @@
 <template>
-  <div>
-    <div w-20 h-20 bg-red top-20 class="box"></div>
-    <div w-full flex>
-      <vue-pdf-embed row :source="source1" w-300 />
-    </div>
+  <div class="app-header">
+    <template v-if="isLoading"> Loading... </template>
+
+    <template v-else>
+      <span v-if="showAllPages"> {{ pageCount }} page(s) </span>
+
+      <span v-else>
+        <button :disabled="page <= 1" @click="page--">❮</button>
+
+        {{ page }} / {{ pageCount }}
+
+        <button :disabled="page >= pageCount" @click="page++">❯</button>
+      </span>
+
+      <label class="right">
+        <input v-model="showAllPages" type="checkbox" />
+
+        Show all pages
+      </label>
+    </template>
+  </div>
+
+  <div class="app-content">
+    <div class="blokje"><input type="text" /></div>
+    <vue-pdf-embed
+      ref="pdfRef"
+      :source="pdfSource"
+      :page="page"
+      @password-requested="handlePasswordRequest"
+      @rendered="handleDocumentRender"
+    />
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script>
 import VuePdfEmbed from 'vue-pdf-embed';
 
-//loop over object
+// OR THE FOLLOWING IMPORT FOR VUE 2
+// import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
 
-const source1 = ref('src\\assets\\Zeewier.pdf');
-
-const count = ref(0);
-const drawing = ref();
-
-const min = -5;
-const max = 5;
+export default {
+  components: {
+    VuePdfEmbed,
+  },
+  data() {
+    return {
+      isLoading: true,
+      page: null,
+      pageCount: 1,
+      pdfSource: 'src/assets/test.pdf',
+      showAllPages: false,
+    };
+  },
+  watch: {
+    showAllPages() {
+      this.page = this.showAllPages ? null : 1;
+    },
+  },
+  methods: {
+    handleDocumentRender(args) {
+      console.log(args);
+      this.isLoading = false;
+      this.pageCount = this.$refs.pdfRef.pageCount;
+    },
+    handlePasswordRequest(callback, retry) {
+      callback(prompt(retry ? 'Enter password again' : 'Enter password'));
+    },
+  },
+};
 </script>
 <style>
-.vierkant {
-  width: 100%;
-  height: 0;
-  padding-top: 100%;
-  border: 1px solid red;
-  max-width: 100px;
+.blokje {
+  position: absolute;
+  z-index: 20;
+  top: 2cm;
+  left: 3cm;
+  width: 2cm;
+  height: 2cm;
+  border: 2px solid blue;
+  fill: gold;
 }
-.test {
-  border: 5px solid;
-  display: flex;
-  height: 150px;
-  align-items: center;
-}
-/* box is floating over pdf*/
 </style>
